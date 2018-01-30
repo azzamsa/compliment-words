d9df2ee433fb1aeaa1708381915301bc82ecbf6e
azzamsa <oktinastuzi@gmail.com>
2018-01-30 10:20:12 +0700

    add slime words

---
+++ slime/slime-word.el
@@ -0,0 +1,12 @@
+(defvar slime-words-of-encouragement
+  `("Let the hacking commence!"
+    "Hacks and glory await!"
+    "Hack and be merry!"
+    "Your hacking starts... NOW!"
+    "May the source be with you!"
+    "Take this REPL, brother, and may it serve you well."
+    "Lemonodor-fame is but a hack away!"
+    "Are we consing yet?"
+    ,(format "%s, this could be the start of a beautiful program."
+             (slime-user-first-name)))
+  "Scientifically-proven optimal words of hackerish encouragement.")
